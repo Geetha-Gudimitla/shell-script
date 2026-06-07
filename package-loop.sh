29c0f67d02ecbc8529dfef4e1138bc9c6f7c9566
Geetha-Gudimitla <geethagudimitla7@gmail.com>
2026-06-07 19:57:13 +0530

    updated config changes

--- package-loop.sh
+++ package-loop.sh
@@ -3,14 +3,16 @@ USERID=$(id -u)
 if [$? -ne 0]
 then
    echo "Need Sudo access"
+   exit 1
 else
   dnf list installed mysql 
-  if [? -ne 0]
+  if [$? -ne 0]
   then 
     dnf install mysql 
     if [$? -ne 0]
      then 
        echo "Installation failed mysql"
+       exit 1
     else
        echo "Installation Success mysql"
     fi
@@ -19,12 +21,13 @@ else
   fi
 
    dnf list installed git
-  if [? -ne 0]
+  if [$? -ne 0]
   then 
     dnf install git 
     if [$? -ne 0]
      then 
        echo "Installation failed git"
+       exit 1
     else
        echo "Installation Success git "
     fi
